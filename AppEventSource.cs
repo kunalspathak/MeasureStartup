0f07f7ce2377cdffc201624b2754502e5c3b6ffd
Kunal Pathak <Kunal.Pathak@microsoft.com>
2022-04-29 11:46:31 -0700

    Rename KPathakTrace to PaintDotnetTrace

--- AppEventSource.cs
+++ AppEventSource.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Tracing;
 
-[EventSource(Name = "KPathakTrace")]
+[EventSource(Name = "PaintDotnetTrace")]
 public sealed class AppEventSource : EventSource
 {
     public static AppEventSource Log = new AppEventSource();
@@ -10,4 +10,23 @@ public sealed class AppEventSource : EventSource
     // must increment with each logging method.
     [Event(1)]
     public void AppLaunched(int iterNumber) { WriteEvent(1, $"Iter{iterNumber}"); }
+
+    // The numbers passed to WriteEvent and EventAttribute
+    // must increment with each logging method.
+    [Event(2)]
+    public void AppStarted() { WriteEvent(2, ""); }
+
+    [Event(3)]
+    public void AppReady() { WriteEvent(3, ""); }
+
+    [Event(4)]
+    public void AppIdle()
+    {
+        if (firstIdleLogged)
+        {
+            return;
+        }
+        WriteEvent(4, "");
+        firstIdleLogged = true;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,51 +2,116 @@
 using System.Diagnostics;
 using System.Threading;
 
-public class Program
+//public class Program
+//{
+
+//    public static int Main(string[] args)
+//    {
+//        if (args.Length != 4)
+//        {
+//            Console.WriteLine("MeasureStartup.exe <path_to_exe> <args_to_exe> <iterations> <delay_in_secs>");
+//            return 1;
+//        }
+
+//        string path = args[0];
+//        string arguments = args[1];
+//        int numIterations = int.Parse(args[2]);
+//        int delayInSecs = int.Parse(args[3]);
+
+//        Console.WriteLine($"Launching '{path} {arguments}' {numIterations} times with wait time before exit set to {delayInSecs} secs.");
+//        for (int i = 0; i < numIterations; i++)
+//        {
+//            StartProcess(i,  delayInSecs, path, arguments);
+//        }
+//        return 0;
+//    }
+
+
+//    public static void StartProcess(int iteration, int delayInSecs, string path, string args = "")
+//    {
+//        var psi = new ProcessStartInfo(path);
+//        psi.Arguments = args;
+//        psi.UseShellExecute = false;
+
+//        try
+//        {
+//            var process = new Process();
+//            process.StartInfo = psi;
+//            process.EnableRaisingEvents = true; //? TODO: Needed?
+
+//            process.Start();
+//            AppEventSource.Log.AppLaunched(iteration);
+
+
+//            Console.WriteLine($"{iteration} Process '{process.Id}' launched");
+//            Thread.Sleep(delayInSecs * 1000);
+//            process.Kill();
+//        }
+//        catch (Exception ex)
+//        {
+//            Debug.WriteLine($"Failed to start new app instance: {ex}");
+//        }
+//    }
+//}
+
+internal enum ApplicationState
 {
+    Unknown = 0,
+    Starting,
+    Running,
+    Closing,
+    Exiting
+}
 
-    public static int Main(string[] args)
+namespace PaintDotNet
+{
+    class Program
     {
-        if (args.Length != 4)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("MeasureStartup.exe <path_to_exe> <args_to_exe> <iterations> <delay_in_secs>");
-            return 1;
+            AppEventSource.Log.AppStarted();
+            // ...
+            // Initialization code
+            // ...
+            new Program().Start();
         }
 
-        string path = args[0];
-        string arguments = args[1];
-        int numIterations = int.Parse(args[2]);
-        int delayInSecs = int.Parse(args[3]);
-
-        Console.WriteLine($"Launching '{path} {arguments}' {numIterations} times with wait time before exit set to {delayInSecs} secs.");
-        for (int i = 0; i < numIterations; i++)
+        public void Start()
         {
-            StartProcess(i,  delayInSecs, path, arguments);
+            // More initialization code
+            State = ApplicationState.Running;
+            try
+            {
+                // PdnBaseForm.PushModalLoopCount();
+                AppEventSource.Log.AppReady();
+                Application.Run(this.mainForm);
+            }
+            finally
+            {
+                State = ApplicationState.Closing;
+            }
         }
-        return 0;
+
+        private void OnApplicationIdle(object sender, EventArgs e)
+        {
+            // Log when the 1st time application becomes idle
+            AppEventSource.Log.AppIdle();
+        }
+
+
+
+
+
+            static ApplicationState State;
+        object mainForm;
+
     }
 
-
-    public static void StartProcess(int iteration, int delayInSecs, string path, string args = "")
+    static class Application
     {
-        var psi = new ProcessStartInfo(path);
-        psi.Arguments = args;
-        psi.UseShellExecute = false;
+        public static void Run(object t)
+        {
 
-        try
-        {
-            var process = new Process();
-            process.StartInfo = psi;
-            process.EnableRaisingEvents = true; //? TODO: Needed?
-            process.Start();
-            AppEventSource.Log.AppLaunched(iteration);
-            Console.WriteLine($"{iteration} Process '{process.Id}' launched");
-            Thread.Sleep(delayInSecs * 1000);
-            process.Kill();
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"Failed to start new app instance: {ex}");
         }
     }
 }
